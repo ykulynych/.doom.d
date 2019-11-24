fd7ed7dd6d16685f4155ca1df2ae3ead466cd5ac
Yura <yura.kulinich.ua@gmail.com>
2019-11-24 18:02:10 +0200

    Added packages and setup config for web development

--- config.el
+++ config.el
@@ -2,4 +2,30 @@
 
 ;; Place your private configuration here
 (setq
- projectile-project-search-path '("~/Documents/work/"))
+ projectile-project-search-path '("~/Documents/work/")
+ web-mode-markup-indent-offset 2
+ web-mode-code-indent-offset 2
+ web-mode-css-indent-offset 2
+ js-indent-level 2
+ typescript-indent-level 2
+ json-reformat:indent-width 2
+ prettier-js-args '("--single-quote")
+ dired-dwim-target t
+ css-indent-offset 2
+ +magit-hub-features t)
+
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+(add-hook 'flycheck-mode-hook 'add-node-modules-path)
+
+(add-hook!
+  js2-mode 'prettier-js-mode
+  (add-hook 'before-save-hook #'refmt-before-save nil t))
+
+(add-hook!
+  web-mode-hook 'prettier-js-mode
+  (add-hook 'before-save-hook #'refmt-before-save nil t))
+
+(eval-after-load 'web-mode
+    '(progn
+       (add-hook 'web-mode-hook #'add-node-modules-path)
+       (add-hook 'web-mode-hook #'prettier-js-mode)))
--- packages.el
+++ packages.el
@@ -1,7 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; .doom.d/packages.el
 
-;;; Examples:
-;; (package! some-package)
-;; (package! another-package :recipe (:host github :repo "username/repo"))
-;; (package! builtin-package :disable t)
+(package! ob-typescript)
+(package! indium)
+(package! prettier-js)
+(package! move-text)
+(package! vue-mode)
+(package! add-node-modules-path)
